9ab47193b539fd77f6b5f23b9534f84eb171daa2
edgOnGithub <e.m.jee@lse.ac.uk>
2018-02-14 23:56:04 +0000

    Finished a run of horseshoe model

--- DissertationAnalysisScript2.R
+++ DissertationAnalysisScript2.R
@@ -30,6 +30,39 @@ prior_coeff <- hs(df = 1, global_df = 1, global_scale = tau0)
 
 fit2 <- stan_glm(full.model, family = gaussian(), data = covariates.u,
                  prior = prior_coeff,
-                 adapt_delta = 0.999999)
+                 adapt_delta = 0.999, QR = TRUE, control = list(max_treedepth = 20))
 beepr::beep()
-launch_shinystan(fit2)
+save(fit2, file = 'Stan_covariate_fit.RData')
+
+fit <- fit2
+fit <- varsel(fit, method='forward')
+varsel_stats(fit)
+
+varsel_plot(fit, stats=c('mlpd', 'mse'))
+fit_cv <- cv_varsel(fit, method='forward', cv_method='LOO')
+fit_cv$varsel$ssize
+mcmc_areas(as.matrix(fit), 
+           pars = c('(Intercept)', names(fit$varsel$vind[1:3]), 'sigma')) + coord_cartesian(xlim = c(-2, 2))
+
+proj <- project(fit, nv = 3, ns = 800)
+mcmc_areas(as.matrix(proj)) + coord_cartesian(xlim = c(-2, 2))
+
+
+########
+covariates.ar <- inner_join(x = CAR.10.unfiltered, y = terror.covariates.subset, by = c('market.date' = 'Date')) %>% 
+  subset(select = -c(Date, event.car, event.t.stat, event.p.value, event.confidence.interval, stars, market.date)) %>% 
+  as.tibble
+
+
+full.model2 <- event.ar ~ .
+
+
+
+n <- nrow(terror.covariates.subset)
+D <- ncol(terror.covariates.subset)
+
+fit3 <- stan_glm(full.model2, family = gaussian(), data = covariates.ar,
+                 prior = prior_coeff,
+                 adapt_delta = 0.999, QR = TRUE, control = list(max_treedepth = 20))
+
+save(fit3, file = 'Stan_covariate_fit_ar.RData')
