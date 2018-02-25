641029d0777e8d01af8e58aa89e9694dc5cc7f25
edgOnGithub <34009555+edgOnGithub@users.noreply.github.com>
2018-02-25 11:18:03 +0000

    Added some CAR4 Plots

--- Exploratory Heterogenous analysis.R
+++ Exploratory Heterogenous analysis.R
@@ -1,28 +1,32 @@
+rm(list = ls())
+
 library(rstanarm)
 library(broom)
 library(projpred)
-library
-load('AWS Output/Current Attempt/OLS_fit_CAR4_f.Rdata')
+library(bayesplot)
+load('AWS Output/CAR4/OLS_fit_CAR4_f.Rdata')
 launch_shinystan(OLS.fit.CAR4.f)
 
-load('AWS Output/Current Attempt/LASSO_fit_CAR4_f.Rdata')
+plot(OLS.fit.CAR4.f)
+
+load('AWS Output/CAR4/LASSO_fit_CAR4_f.Rdata')
 launch_shinystan(laplace.fit.CAR4.f)
+
+plot(laplace.fit.CAR4.f)
 
 fit_v <- varsel(laplace.fit.CAR4.f)
 varsel_plot(fit_v)
 
-fit_cv <- cv_varsel(laplace.fit.CAR4.f)
-fit_cv
-fit_cv$varsel$ssize
+
 fit_v$varsel$ssize
 
 
-mcmc_areas(as.matrix(fit_cv), 
-           pars = c('(Intercept)', names(fit_cv$varsel$vind[1:3]), 'sigma')) + coord_cartesian(xlim = c(-2, 2))
-load('AWS Output/Current Attempt/HS_CAR4_f_fit.RData')
+mcmc_areas(as.matrix(fit_v), 
+           pars = c('(Intercept)', names(fit_v$varsel$vind[1:5]), 'sigma')) + coord_cartesian(xlim = c(-2, 2))
+load('AWS Output/CAR4/HS_CAR4_f_fit.RData')
 launch_shinystan(CAR.4.f.fit)
 
-load('AWS Output/Current Attempt/LASSO_fit_R_f.Rdata')
+load('AWS Output/CAR4/LASSO_fit_R_f.Rdata')
 launch_shinystan(laplace.fit.R.f)
 
 
