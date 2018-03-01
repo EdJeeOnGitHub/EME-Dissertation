dfd7ee0f3522a34b31ee84cebdb8eb7e52dad289
edgOnGithub <34009555+edgOnGithub@users.noreply.github.com>
2018-03-01 20:02:40 +0000

    Removed unneeded files and added more plots to hetero analysis

--- Exploratory Heterogenous analysis.R
+++ Exploratory Heterogenous analysis.R
@@ -1,37 +1,125 @@
 rm(list = ls())
+dev.off()
 
 library(rstanarm)
 library(broom)
 library(projpred)
 library(bayesplot)
+#OLS
 load('AWS Output/CAR4/OLS_fit_CAR4_f.Rdata')
-launch_shinystan(OLS.fit.CAR4.f)
-
-plot(OLS.fit.CAR4.f)
-
+# load('AWS Output/CAR4/OLS_fit_CAR4_u.Rdata')
+#LASSO
+# load('AWS Output/CAR4/LASSO_fit_CAR4_u.Rdata')
 load('AWS Output/CAR4/LASSO_fit_CAR4_f.Rdata')
-launch_shinystan(laplace.fit.CAR4.f)
 
-plot(laplace.fit.CAR4.f)
-
-fit_v <- varsel(laplace.fit.CAR4.f)
-varsel_plot(fit_v)
-
-
-fit_v$varsel$ssize
-
-
-mcmc_areas(as.matrix(fit_v), 
-           pars = c('(Intercept)', names(fit_v$varsel$vind[1:5]), 'sigma')) + coord_cartesian(xlim = c(-2, 2))
-load('AWS Output/CAR4/HS_CAR4_f_fit.RData')
-launch_shinystan(CAR.4.f.fit)
-
-load('AWS Output/CAR4/LASSO_fit_R_f.Rdata')
-launch_shinystan(laplace.fit.R.f)
-
-
-R_vs <- varsel(laplace.fit.R.f)
-varsel_plot(R_vs)
-mcmc_areas(as.matrix(R_vs),
-           pars = c('(Intercept)', names(R_vs$varsel$vind[1:3]), 'sigma')) + coord_cartesian(xlim = c(-2, 2))
+# OLS and LASSO Returns
+load('AWS Output/CAR4/ols_fit_R_u.Rdata')
+load('AWS Output/CAR4/LASSO_fit_R_u.Rdata')
+non.count.vector <- c(
+  "(Intercept)",
+  "multiple",
+  "success",
+  "guncertain1",
+  "nperps",
+  "nperpcap",
+  "nkill",
+  "nwound",
+  "nwoundte" ,
+  "property" ,
+  "ishostkid",
+  "nhostkid",
+  "INT_IDEO",
+  "incident",
+  'MA4',
+  'T'
 )
+
+## OLS CAR4 filtered ####
+
+CAR4.f.plot.1 <- plot(OLS.fit.CAR4.f, pars = non.count.vector) +
+  ggtitle('CAR4 Determinants', subtitle = 'Filtered Events')
+
+CAR4.f.plot.2 <- plot(OLS.fit.CAR4.f, regex_pars =  c('attack', 'prov')) +
+  ggtitle('CAR4 Determinants', subtitle = 'Filtered Events')
+CAR4.f.plot.3 <- plot(OLS.fit.CAR4.f, regex_pars = 'target') +
+  ggtitle('CAR4 Determinants', subtitle = 'Filtered Events')
+CAR4.f.plot.4 <- plot(OLS.fit.CAR4.f, regex_pars = 'weap') +
+  ggtitle('CAR4 Determinants', subtitle = 'Filtered Events')
+
+
+CAR4.f.plot.1
+CAR4.f.plot.2
+CAR4.f.plot.3
+CAR4.f.plot.4
+
+## CAR4 Filtered LASSO ####
+CAR4.f.laplace.plot.1 <- plot(laplace.fit.CAR4.f, pars = non.count.vector) +
+  ggtitle('CAR4 Determinants', subtitle = 'Filtered Events, LASSO')
+
+CAR4.f.laplace.plot.2 <- plot(laplace.fit.CAR4.f, regex_pars =  c('attack', 'prov')) +
+  ggtitle('CAR4 Determinants', subtitle = 'Filtered Events, LASSO')
+CAR4.f.laplace.plot.3 <- plot(laplace.fit.CAR4.f, regex_pars = 'target') +
+  ggtitle('CAR4 Determinants', subtitle = 'Filtered Events, LASSO')
+CAR4.f.laplace.plot.4 <- plot(laplace.fit.CAR4.f, regex_pars = 'weap') +
+  ggtitle('CAR4 Determinants', subtitle = 'Filtered Events, LASSO')
+
+CAR4.varsel <- varsel(laplace.fit.CAR4.f)
+CAR4.var.selected.fit <- mcmc_areas(as.matrix(CAR4.varsel), 
+                                    pars = c('(Intercept)', names(CAR4.varsel$varsel$vind[1:5]))) + coord_cartesian(xlim = c(-2, 2)) +
+  ggtitle('Variables with greatest predictive power - CAR4')
+
+CAR4.f.laplace.plot.1
+CAR4.f.laplace.plot.2
+CAR4.f.laplace.plot.3
+CAR4.f.laplace.plot.4
+CAR4.var.selected.fit
+
+
+## Returns OLS ####
+non.count.vector2 <- replace(non.count.vector, non.count.vector == 'MA4', '`number of articles`')
+
+R.f.plot.1 <- plot(ols.fit.R.u, pars = non.count.vector2) +
+  ggtitle('Terror Day Return Determinants', subtitle = 'All Events')
+
+R.f.plot.2 <- plot(ols.fit.R.u, regex_pars =  c('attack', 'prov')) +
+  ggtitle('Terror Day Return Determinants', subtitle = 'All Events')
+R.f.plot.3 <- plot(ols.fit.R.u, regex_pars = 'target') +
+  ggtitle('Terror Day Return Determinants', subtitle = 'All Events')
+R.f.plot.4 <- plot(ols.fit.R.u, regex_pars = 'weap') +
+  ggtitle('Terror Day Return Determinants', subtitle = 'All Events')
+
+
+R.f.plot.1
+R.f.plot.2
+R.f.plot.3
+R.f.plot.4
+
+
+## Returns LASSO ####
+laplace.f.plot.1 <- plot(laplace.fit.R.u, pars = non.count.vector2) +
+  ggtitle('Terror Day Return Determinants', subtitle = 'All Events, LASSO')
+
+laplace.f.plot.2 <- plot(laplace.fit.R.u, regex_pars =  c('attack', 'prov')) +
+  ggtitle('Terror Day Return Determinants', subtitle = 'All Events, LASSO')
+laplace.f.plot.3 <- plot(laplace.fit.R.u, regex_pars = 'target') +
+  ggtitle('Terror Day Return Determinants', subtitle = 'All Events, LASSO')
+laplace.f.plot.4 <- plot(laplace.fit.R.u, regex_pars = 'weap') +
+  ggtitle('Terror Day Return Determinants', subtitle = 'All Events, LASSO')
+
+laplace.R.varsel <- varsel(laplace.fit.R.u)
+laplace.R.var.selected.fit <- mcmc_areas(as.matrix(laplace.R.varsel), 
+                                    pars = c('(Intercept)', names(laplace.R.varsel$varsel$vind[1:5]))) + coord_cartesian(xlim = c(-0.05, 0.05)) +
+  ggtitle('Variables with greatest predictive power - Terror Day Returns')
+laplace.f.plot.1
+laplace.f.plot.2
+laplace.f.plot.3
+laplace.f.plot.4
+laplace.R.var.selected.fit
+
+
+
+## Notes: Have 51 predictors and only 88 observations of filtered events.
+## Variable selection techniques with CAR4 data suggest wounded and media intensity biggest determinants but don't have enough power to claim they're significantly different from 0.
+## Variable selection with terror return data indicates weaptype are biggest culprits but again very close to 0 - not sure how to interpret this.
+## Assuming events are iid - could possibly look into relaxing this assumption?
+
