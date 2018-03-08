29b689c4925351dacbf60c8f86c7d1d8e75be709
edgOnGithub <34009555+edgOnGithub@users.noreply.github.com>
2018-03-08 14:05:35 +0000

    Checked for overlapping events per decade.
    Added CAAR4 by decade

--- AnalysisScripts/DissertationAnalysisScript1.R
+++ AnalysisScripts/DissertationAnalysisScript1.R
@@ -244,29 +244,29 @@ FTSE100.finite.moment.pvalue
 
 #### Decade CAR Results ####
 
-# CAR10
+decade.event.overlap.check <- list()
+for (i in 1:4){
+  events.screened <- screen.overlapping.events(events.decade.list[[i]], arrange = FALSE)
+  decade.event.overlap.check[[i]] <- events.screened
+}
+## There's no overlapping of events WITHIN the top 5 events each decade. N.B. The first event cannot overlap by construction so gives NA
 
+# CAR10
 decade.event.study.CAR10 <- events.decade.list %>% 
-  map_dfr(calculate.car, index = index.zoo.UK.ALLSHARE.omitted, boot = TRUE)
+  map_dfr(calculate.car, index = index.zoo.UK.ALLSHARE.omitted, boot = FALSE)
 
 
 # CAR4 N.B. car.length set to 5 as it counts event day (i.e. t=0) inclusively
 
 decade.event.study.CAR4 <- events.decade.list %>% 
-  map_dfr(calculate.car, index = index.zoo.UK.ALLSHARE.omitted, car.length = 5)
+  map_dfr(calculate.car, index = index.zoo.UK.ALLSHARE.omitted, car.length = 5, boot = TRUE)
 
 
 
 decade.event.study.CAR10
 decade.event.study.CAR4
 # Calculating CAAR split by decade
-# 80s
-CAAR.decade.80s <- calculate.car(events.80s,
-                                             index.zoo.UK.ALLSHARE.omitted) %>% 
-  calculate.rolling.CAAR %>% 
-  calculate.CI.rolling.CAAR %>% 
-  select( c(rolling.CAAR, df, rolling.sd, rolling.ci)) %>% 
-  .[5,]
+
   
 
 # All of them together
@@ -284,6 +284,22 @@ CAAR.10.by.decade$Decade <- CAAR.10.by.decade$Date %>%
 CAAR.10.by.decade <- select(CAAR.10.by.decade, c(rolling.CAAR, rolling.sd, rolling.ci, Decade))
 colnames(CAAR.10.by.decade) <- c('CAAR', 'SD', 'CI', 'Decade')
 CAAR.10.by.decade
+
+# Now CAAR.4
+CAAR.4.by.decade <- events.decade.list %>% 
+  map( calculate.car, index = index.zoo.UK.ALLSHARE.omitted, car.length = 5) %>% 
+  map(calculate.rolling.CAAR) %>% 
+  map(calculate.CI.rolling.CAAR) %>% 
+  map_dfr(. %>% filter(n ==5))
+
+CAAR.4.by.decade$Decade <- CAAR.4.by.decade$Date %>%
+  as.Date %>%
+  floor_date(years(4)) %>%
+  year
+
+CAAR.4.by.decade <- select(CAAR.4.by.decade, c(rolling.CAAR, rolling.sd, rolling.ci, Decade))
+colnames(CAAR.4.by.decade) <- c('CAAR', 'SD', 'CI', 'Decade')
+CAAR.4.by.decade
 
 #### Decade Logit Results ####
 
